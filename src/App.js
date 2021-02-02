3ca3af752ae5b8d4292af6a71344aa70450fcea3
muhammadmb <muhammadmagdyje@gmail.com>
2021-02-02 08:30:34 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import NavigationBar from './Components/NavigationBar';
+import {BrowserRouter,Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <div className = "App">
+      <BrowserRouter>
+        <NavigationBar/>
+      
+      
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
